4abce4ab3646f18d4ddefd3cfdcba9a63588f2e3
Felipe Quintella <felipe.quintella@fgv.br>
2014-11-26 20:14:30 -0200

    FEATURE: Migrating to the twig template engine

--- webapp/admin/custom_names.php
+++ webapp/admin/custom_names.php
@@ -1,245 +1,142 @@
 <?php
-        /* This Source Code Form is subject to the terms of the Mozilla Public
-         * License, v. 2.0. If a copy of the MPL was not distributed with this
-         * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-        // Include required functions file
-        require_once(realpath(__DIR__ . '/../includes/functions.php'));
-        require_once(realpath(__DIR__ . '/../includes/authenticate.php'));
-	require_once(realpath(__DIR__ . '/../includes/display.php'));
+// Include required functions file
+require_once(realpath(__DIR__ . '/../includes/libs.php'));
+require_once(realpath(__DIR__ . '/../includes/display.php'));
 
-        // Include Zend Escaper for HTML Output Encoding
-        require_once(realpath(__DIR__ . '/../includes/Component_ZendEscaper/Escaper.php'));
-        $escaper = new Zend\Escaper\Escaper('utf-8');
+// Include Zend Escaper for HTML Output Encoding
+require_once(realpath(__DIR__ . '/../includes/Component_ZendEscaper/Escaper.php'));
+$escaper = new Zend\Escaper\Escaper('utf-8');
 
-        // Add various security headers
-        header("X-Frame-Options: DENY");
-        header("X-XSS-Protection: 1; mode=block");
+// Add various security headers
+header("X-Frame-Options: DENY");
+header("X-XSS-Protection: 1; mode=block");
 
-        // If we want to enable the Content Security Policy (CSP) - This may break Chrome
-        if (CSP_ENABLED == "true")
-        {
-                // Add the Content-Security-Policy header
-                header("Content-Security-Policy: default-src 'self'; script-src 'unsafe-inline'; style-src 'unsafe-inline'");
-        }
-
-        // Session handler is database
-        if (USE_DATABASE_FOR_SESSIONS == "true")
-        {
-		session_set_save_handler('sess_open', 'sess_close', 'sess_read', 'sess_write', 'sess_destroy', 'sess_gc');
-        }
-
-        // Start the session
-	session_set_cookie_params(0, '/', '', isset($_SERVER["HTTPS"]), true);
-        session_start('SimpleRisk');
-
-        // Include the language file
-        require_once(language_file());
-
-        require_once(realpath(__DIR__ . '/../includes/csrf-magic/csrf-magic.php'));
-
-        // Check for session timeout or renegotiation
-        session_check();
-
-        // Check if access is authorized
-        if (!isset($_SESSION["access"]) || $_SESSION["access"] != "granted")
-        {
-                header("Location: ../index.php");
-                exit(0);
-        }
-
-	// Default is no alert
-	$alert = false;
-
-        // Check if access is authorized
-        if (!isset($_SESSION["admin"]) || $_SESSION["admin"] != "1")
-        {
-                header("Location: ../index.php");
-                exit(0);
-        }
-
-        // Check if the impact update was submitted
-        if (isset($_POST['update_impact']))
-        {
-                $new_name = $_POST['new_name'];
-                $value = (int)$_POST['impact'];
-
-                // Verify value is an integer
-                if (is_int($value))
-                {
-                        update_table("impact", $new_name, $value);
-
-                        // Audit log
-                        $risk_id = 1000;
-                       $message = "The impact naming convention was modified by the \"" . $_SESSION['user'] . "\" user.";
-                        write_log($risk_id, $_SESSION['uid'], $message);
-
-			// There is an alert message
-			$alert = "good";
-			$alert_message = "The impact naming convention was updated successfully.";
-                }
-        }
-
-        // Check if the likelihood update was submitted
-        if (isset($_POST['update_likelihood']))
-        {
-                $new_name = $_POST['new_name'];
-                $value = (int)$_POST['likelihood'];
-
-                // Verify value is an integer
-                if (is_int($value))
-                {
-                        update_table("likelihood", $new_name, $value);
-
-                        // Audit log
-                        $risk_id = 1000;
-                       $message = "The likelihood naming convention was modified by the \"" . $_SESSION['user'] . "\" user.";
-                        write_log($risk_id, $_SESSION['uid'], $message);
-
-			// There is an alert message
-                        $alert = "good";
-                        $alert_message = "The likelihood naming convention was updated successfully.";
-                }
-        }
-
-        // Check if the mitigation effort update was submitted
-        if (isset($_POST['update_mitigation_effort']))
-        {
-                $new_name = $_POST['new_name'];
-                $value = (int)$_POST['mitigation_effort'];
-
-                // Verify value is an integer
-                if (is_int($value))
-                {
-                        update_table("mitigation_effort", $new_name, $value);
-
-                        // Audit log
-                        $risk_id = 1000;
-                       $message = "The mitigation effort naming convention was modified by the \"" . $_SESSION['user'] . "\" user.";
-                        write_log($risk_id, $_SESSION['uid'], $message);
-
-			// There is an alert message
-                        $alert = "good";
-                        $alert_message = "The mitigation effort naming convention was updated successfully.";
-                }
-        }
-?>
-
-<!doctype html>
-<html>
-  
-  <head>
-    <script src="../js/jquery.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <title>SimpleRisk: Enterprise Risk Management Simplified</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/bootstrap-responsive.css"> 
-  </head>
-  
-  <body>
-    <title>SimpleRisk: Enterprise Risk Management Simplified</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/bootstrap-responsive.css">
-    <link rel="stylesheet" href="../css/divshot-util.css">
-    <link rel="stylesheet" href="../css/divshot-canvas.css">
-    <link rel="stylesheet" href="../css/display.css">
-    <div class="navbar">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="brand" href="http://www.simplerisk.org/">SimpleRisk</a>
-          <div class="navbar-content">
-            <ul class="nav">
-              <li>
-                <a href="../index.php"><?php echo $escaper->escapeHtml($lang['Home']); ?></a> 
-              </li>
-              <li>
-                <a href="../management/index.php"><?php echo $escaper->escapeHtml($lang['RiskManagement']); ?></a> 
-              </li>
-              <li>
-                <a href="../reports/index.php"><?php echo $escaper->escapeHtml($lang['Reporting']); ?></a> 
-              </li>
-              <li class="active">
-                <a href="index.php"><?php echo $escaper->escapeHtml($lang['Configure']); ?></a>
-              </li>
-            </ul>
-          </div>
-<?php
-if (isset($_SESSION["access"]) && $_SESSION["access"] == "granted")
-{
-          echo "<div class=\"btn-group pull-right\">\n";
-          echo "<a class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">" . $escaper->escapeHtml($_SESSION['name']) . "<span class=\"caret\"></span></a>\n";
-          echo "<ul class=\"dropdown-menu\">\n";
-          echo "<li>\n";
-          echo "<a href=\"../account/profile.php\">" . $escaper->escapeHtml($lang['MyProfile']) . "</a>\n";
-          echo "</li>\n";
-          echo "<li>\n";
-          echo "<a href=\"../logout.php\">" . $escaper->escapeHtml($lang['Logout']) . "</a>\n";
-          echo "</li>\n";
-          echo "</ul>\n";
-          echo "</div>\n";
+// If we want to enable the Content Security Policy (CSP) - This may break Chrome
+if (CSP_ENABLED == "true") {
+    // Add the Content-Security-Policy header
+    header("Content-Security-Policy: default-src 'self'; script-src 'unsafe-inline'; style-src 'unsafe-inline'");
 }
-?>
-        </div>
-      </div>
-    </div>
-<?php
-        if ($alert == "good")
-        {
-                echo "<div id=\"alert\" class=\"container-fluid\">\n";
-                echo "<div class=\"row-fluid\">\n";
-                echo "<div class=\"span12 greenalert\">" . $escaper->escapeHtml($alert_message) . "</div>\n";
-                echo "</div>\n";
-                echo "</div>\n";
-                echo "<br />\n";
-        }
-        else if ($alert == "bad")
-        {
-                echo "<div id=\"alert\" class=\"container-fluid\">\n";
-                echo "<div class=\"row-fluid\">\n";
-                echo "<div class=\"span12 redalert\">" . $escaper->escapeHtml($alert_message) . "</div>\n";
-                echo "</div>\n";
-                echo "</div>\n";
-                echo "<br />\n";
-        }
-?>
-    <div class="container-fluid">
-      <div class="row-fluid">
-        <div class="span3">
-          <?php view_configure_menu("RedefineNamingConventions"); ?>
-        </div>
-        <div class="span9">
-          <div class="row-fluid">
-            <div class="span12">
-              <div class="hero-unit">
-                <form name="impact" method="post" action="">
-                <p>
-                <h4><?php echo $escaper->escapeHtml($lang['Impact']); ?>:</h4>
-                <?php echo $escaper->escapeHtml($lang['Change']); ?> <?php create_dropdown("impact") ?> <?php echo $escaper->escapeHtml($lang['to']); ?> <input name="new_name" type="text" size="20" />&nbsp;&nbsp;<input type="submit" value="<?php echo $escaper->escapeHtml($lang['Update']); ?>" name="update_impact" /></p>
-                </form>
-              </div>
-              <div class="hero-unit">
-                <form name="likelihood" method="post" action="">
-                <p>
-                <h4><?php echo $escaper->escapeHtml($lang['Likelihood']); ?>:</h4>
-                <?php echo $escaper->escapeHtml($lang['Change']); ?> <?php create_dropdown("likelihood") ?> <?php echo $escaper->escapeHtml($lang['to']); ?> <input name="new_name" type="text" size="20" />&nbsp;&nbsp;<input type="submit" value="<?php echo $escaper->escapeHtml($lang['Update']); ?>" name="update_likelihood" /></p>
-                </form>
-              </div>
-              <div class="hero-unit">
-                <form name="mitigation_effort" method="post" action="">
-                <p>
-                <h4><?php echo $escaper->escapeHtml($lang['MitigationEffort']); ?>:</h4>
-                <?php echo $escaper->escapeHtml($lang['Change']); ?> <?php create_dropdown("mitigation_effort") ?> <?php echo $escaper->escapeHtml($lang['to']); ?> <input name="new_name" type="text" size="20" />&nbsp;&nbsp;<input type="submit" value="<?php echo $escaper->escapeHtml($lang['Update']); ?>" name="update_mitigation_effort" /></p>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
 
-</html>
+// Session handler is database
+if (USE_DATABASE_FOR_SESSIONS == "true") {
+    session_set_save_handler('sess_open', 'sess_close', 'sess_read', 'sess_write', 'sess_destroy', 'sess_gc');
+}
+
+// Start the session
+session_set_cookie_params(0, '/', '', isset($_SERVER["HTTPS"]), true);
+session_start('LessRisk');
+
+// Include the language file
+require_once(language_file());
+
+require_once(realpath(__DIR__ . '/../includes/csrf-magic/csrf-magic.php'));
+
+// Check for session timeout or renegotiation
+session_check();
+
+// Check if access is authorized
+if (!isset($_SESSION["access"]) || $_SESSION["access"] != "granted") {
+    header("Location: ../index.php");
+    exit(0);
+}
+
+// Default is no alert
+$alert = false;
+
+// Check if access is authorized
+if (!isset($_SESSION["admin"]) || $_SESSION["admin"] != "1") {
+    header("Location: ../index.php");
+    exit(0);
+}
+
+// Check if the impact update was submitted
+if (isset($_POST['update_impact'])) {
+    $new_name = $_POST['new_name'];
+    $value = (int)$_POST['impact'];
+
+    // Verify value is an integer
+    if (is_int($value)) {
+        update_table("impact", $new_name, $value);
+
+        // Audit log
+        $risk_id = 1000;
+        $message = "The impact naming convention was modified by the \"" . $_SESSION['user'] . "\" user.";
+        write_log($risk_id, $_SESSION['uid'], $message);
+
+        // There is an alert message
+        $alert = "good";
+        $alert_message = "The impact naming convention was updated successfully.";
+    }
+}
+
+// Check if the likelihood update was submitted
+if (isset($_POST['update_likelihood'])) {
+    $new_name = $_POST['new_name'];
+    $value = (int)$_POST['likelihood'];
+
+    // Verify value is an integer
+    if (is_int($value)) {
+        update_table("likelihood", $new_name, $value);
+
+        // Audit log
+        $risk_id = 1000;
+        $message = "The likelihood naming convention was modified by the \"" . $_SESSION['user'] . "\" user.";
+        write_log($risk_id, $_SESSION['uid'], $message);
+
+        // There is an alert message
+        $alert = "good";
+        $alert_message = "The likelihood naming convention was updated successfully.";
+    }
+}
+
+// Check if the mitigation effort update was submitted
+if (isset($_POST['update_mitigation_effort'])) {
+    $new_name = $_POST['new_name'];
+    $value = (int)$_POST['mitigation_effort'];
+
+    // Verify value is an integer
+    if (is_int($value)) {
+        update_table("mitigation_effort", $new_name, $value);
+
+        // Audit log
+        $risk_id = 1000;
+        $message = "The mitigation effort naming convention was modified by the \"" . $_SESSION['user'] . "\" user.";
+        write_log($risk_id, $_SESSION['uid'], $message);
+
+        // There is an alert message
+        $alert = "good";
+        $alert_message = "The mitigation effort naming convention was updated successfully.";
+    }
+}
+
+$localvars = array();
+
+if($alert == "good" || $alert == "bad") {
+
+    $localvars['alert'] = true;
+    $localvars['alert_type'] = $alert;
+    $localvars['alert_message'] = $alert_message;
+}
+
+$localvars['html_adm_menu'] = view_configure_menu("RedefineNamingConventions");
+
+$localvars['Impact'] = $lang['Impact'];
+$localvars['Change'] = $lang['Change'];
+$localvars['to'] = $lang['to'];
+$localvars['Update'] = $lang['Update'];
+$localvars['Likelihood'] = $lang['Likelihood'];
+$localvars['MitigationEffort'] = $lang['MitigationEffort'];
+
+
+$localvars['dd_impact'] = create_dropdown("impact");
+$localvars['dd_likelihood'] = create_dropdown("likelihood");
+$localvars['dd_mitigation_effort'] = create_dropdown("mitigation_effort");
+
+
+$template = $twig->loadTemplate('admin_custom_names.html.twig');
+
+$template->display(array_merge($base_twigvars, $localvars));
