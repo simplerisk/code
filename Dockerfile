8c0dd1c4549b70f7e059613cb37f15bd854d23e0
milovanderlinden <milo@dogodigi.net>
2018-09-04 20:43:54 +0200

    Updated for use with docker

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,16 @@ ENV MYSQL_USERNAME simplerisk
 ENV MYSQL_PASSWORD simplerisk
 ENV MYSQL_DATABASE simplerisk
 ENV USE_DATABASE_FOR_SESSIONS true
+
+RUN docker-php-ext-install mysqli pdo_mysql
+
 ADD simplerisk /var/www/html
 ADD docker-entrypoint.sh /docker-entrypoint.sh
 COPY config.env.php /var/www/html/includes/config.php
-# Replace config
 
 RUN chmod 755 /*.sh
 RUN chmod -R 755 /var/www/html
-RUN docker-php-ext-install mysqli pdo_mysql
+
+RUN a2enmod rewrite
 
 CMD ["/docker-entrypoint.sh"]
--- installer/docker-entrypoint.sh
+++ installer/docker-entrypoint.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 
-file="/install"
+file="simplerisk-en.sql"
 if [ -d "$file" ]
 then
 	echo "$file found, exit"
 else
-	echo "$file not found, executing scripts"
-    wget -O installer.tgz https://github.com/simplerisk/installer/blob/master/simplerisk-installer-20180830-001.tgz?raw=true
-    tar -xvzf installer.tgz
-    cd install
-    mysql  --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $MYSQL_DATABASE"
-    mysql  --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} -e "CREATE USER $MYSQL_USER IDENTIFIED BY '$MYSQL_PASSWORD'"
-    mysql  --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION";
-
-    for filename in db/*.sql; do
-        mysql --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} --database=${MYSQL_DATABASE} < "$filename" 
+    #wait for mysql
+    i=0
+    while ! nc $DB_HOST $DB_PORT >/dev/null 2>&1 < /dev/null; do
+    i=`expr $i + 1`
+    if [ $i -ge $WAIT_LOOPS ]; then
+        echo "$(date) - ${DB_HOST}:${DB_PORT} still not reachable, giving up"
+        exit 1
+    fi
+    echo "$(date) - waiting for ${DB_HOST}:${DB_PORT}"
+    sleep $WAIT_SLEEP
     done
+
+    echo "database is available"
+	echo "$file not found, executing script"
+    wget -O simplerisk-en.sql https://raw.githubusercontent.com/simplerisk/database/master/simplerisk-en-20180830-001.sql
+    #wget -O installer.tgz https://github.com/simplerisk/installer/blob/master/simplerisk-installer-20180830-001.tgz?raw=true
+    #tar -xvzf installer.tgz
+    #cd install
+    mysql --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $MYSQL_DATABASE"
+    mysql --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} -e "CREATE USER $MYSQL_USER IDENTIFIED BY '$MYSQL_PASSWORD'"
+    mysql --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION";
+    mysql --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} --database=${MYSQL_DATABASE} < simplerisk-en.sql 
+    #for filename in db/*.sql; do
+    #    mysql --host=${DB_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} --database=${MYSQL_DATABASE} < "$filename" 
+    #done
+    echo "done, exit"
 fi
